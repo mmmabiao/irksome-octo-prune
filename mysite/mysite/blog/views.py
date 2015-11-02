5fe6ff1bbe98f7ed3015dbf93316b940bdea9672
mmmabiao <mabiao@aragoncs.com>
2015-11-02 15:23:10 +0800

    评论写在详情页了

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -1,8 +1,9 @@
 # from django.contrib.auth.decorators import login_required
-from mysite.blog.models import BlogsPost, Comment
-from .forms import BlogsPostForm, CommentForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
+from mysite.blog.models import BlogsPost, Comment
+from .forms import BlogsPostForm, CommentForm
 
 
 # Create your views here.
@@ -30,17 +31,11 @@ def create_blog(request):
 def detail(request, pk):
 	blog = BlogsPost.objects.get(id=pk)
 	mm = Comment.objects.all()
-	return render_to_response('detail.html', {'blog': blog, 'mm': mm}, context_instance=RequestContext(request))
-
-
-def comment(request, pk):
-	s = BlogsPost.objects.get(id=pk)
 	if request.method == 'POST':
 		form = CommentForm(request.POST)
 		if form.is_valid():
 			a = form.cleaned_data['comment']
-			Comment.objects.create(comment=a, bid=s.id, )
-			return detail(request, s.id)
-	else:
-		form = CommentForm()
-	return render_to_response('comments.html', {'form': form}, context_instance=RequestContext(request))
+			Comment.objects.create(comment=a, bid=blog.id, )
+	return render_to_response('detail.html', {'blog': blog, 'mm': mm, 'form': form}, context_instance=RequestContext(request))
+
+
